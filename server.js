b6807a5ed5099b7da1dd00788b134b2c4fb655fc
noldakowski <oldakowski.n@husky.neu.edu>
2017-05-09 21:40:12 -0400

    First Commit

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express'); // creates an instance of the express lib
+var app = express();
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// configure a public directory to host static content
+app.use(express.static(__dirname + '/public')); //anything under the public directory will
+                                                // be static and send it verbatim; slash is now public/
+
+require ("./test/app.js")(app);
+
+var port = process.env.PORT || 3000;
+
+app.listen(port);
