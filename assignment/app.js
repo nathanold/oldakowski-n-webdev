b2c601237ed1eac44a72ca5fa41a96b643fa0cb2
noldakowski <oldakowski.n@husky.neu.edu>
2017-06-13 13:16:42 -0400

    Trying to fix heroku

--- assignment/app.js
+++ assignment/app.js
@@ -8,7 +8,23 @@ require('./services/page.service.server');
 require('./services/website.service.server');
 require('./services/widget.service.server');
 app.get('/hello', sayHello);
+var connectionString = 'mongodb://127.0.0.1:27017/test';
 
-function sayHello(req,res){
+if (process.env.MLAB_USERNAME) {
+    connectionString = process.env.MLAB_USERNAME + ":" +
+        process.env.MLAB_PASSWORD + "@" +
+        process.env.MLAB_HOST + ':' +
+        process.env.MLAB_PORT + '/' +
+        process.env.MLAB_APP_NAME;
+}
+
+var connectionString = 'mongodb://127.0.0.1:27017/test'; // for local
+if (process.env.MLAB_USERNAME) { // check if running remotely
+    var username = process.env.MLAB_USERNAME; // get from environment
+    var password = process.env.MLAB_PASSWORD;
+    connectionString = 'mongodb://' + username + ':' + password;
+    connectionString += '@ds137141.mlab.com:37141/heroku_vj9g0c7t'; // user yours
+}
+function sayHello(req, res) {
     res.send('hey there');
 }
