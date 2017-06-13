3cd8669bf5ae28e3d8e898914a8f01ce7208fbe0
noldakowski <oldakowski.n@husky.neu.edu>
2017-06-13 13:25:33 -0400

    Heroku should work now?

--- assignment/app.js
+++ assignment/app.js
@@ -1,15 +1,7 @@
 console.log("SERVER SIDE!");
 var app = require('../express'); // creates an instance of the express lib
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/webdev_noldakowski');
-mongoose.Promise = require('q').Promise;
-require('./services/user.service.server');
-require('./services/page.service.server');
-require('./services/website.service.server');
-require('./services/widget.service.server');
-app.get('/hello', sayHello);
-var connectionString = 'mongodb://127.0.0.1:27017/test';
-
+var connectionString = 'mongodb://127.0.0.1:27017/webdev_noldakowski';
 if (process.env.MLAB_USERNAME) {
     connectionString = process.env.MLAB_USERNAME + ":" +
         process.env.MLAB_PASSWORD + "@" +
@@ -25,6 +17,15 @@ if (process.env.MLAB_USERNAME) { // check if running remotely
     connectionString = 'mongodb://' + username + ':' + password;
     connectionString += '@ds137141.mlab.com:37141/heroku_vj9g0c7t'; // user yours
 }
+mongoose.connect(connectionString);
+
+mongoose.Promise = require('q').Promise;
+require('./services/user.service.server');
+require('./services/page.service.server');
+require('./services/website.service.server');
+require('./services/widget.service.server');
+app.get('/hello', sayHello);
+
 function sayHello(req, res) {
     res.send('hey there');
 }
